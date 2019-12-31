329b2a6de5eca432720919ce98f4727877f3c094
sneakstarberry <gihong2012@likelion.org>
2019-12-31 14:29:02 +0900

    ?

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gihon\onedrive\syu_code\session\suyeon\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
